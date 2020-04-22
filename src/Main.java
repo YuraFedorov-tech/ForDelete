2ab3cd4fe4c2eb3c288469430b5495c5a825f362
YuraForward <iury.fedoroff2018@yandex.ru>
2020-04-22 22:52:17 +0300

    fdg

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@
  *
  */
 
-import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     static List<User> users=new ArrayList<>();
@@ -17,15 +17,16 @@ public class Main {
     public static void main(String[] args) {
         getUsers();
         System.out.println(users);
-     //   List<User> users2=
-
+        List<User> users2=users.stream().filter(x -> x.isApprove()).collect(Collectors.toList());
+        System.out.println(users2);
+        users.stream().filter(x -> x.isApprove()).forEach(System.out::println);
     }
 
     private static void getUsers() {
         for (int i=0;i<10;i++){
             String s=Integer.toString(i);
             boolean q=true;
-            if(i%2==0){
+            if(i%3!=0){
                 q=false;
             }
             users.add(new User(s,q));
